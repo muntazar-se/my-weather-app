8595e45439a98ec78982ba9f44cb4e2c046842fd
muntazarr <145792920+muntazarr@users.noreply.github.com>
2023-11-27 16:18:36 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import WeatherApp from "./Components/Weather App/WeatherApp";
+
+export default function App() {
+
+  return (
+    <div className='App'>
+        <WeatherApp/>
+    </div>
+  )
+}
+
